6bca2c5ab83828174df0de384012587e0538ae8d
pedrofortunato2804 <pedrofort28@gmail.com>
2022-05-17 11:49:29 -0300

    exercicio01

---
+++ exercicio01/exercicio01.js
@@ -0,0 +1,32 @@
+document.getElementById("um").onclick = () => {
+    let displayAppears = document.getElementById("div1").style.display;
+    if(displayAppears === "none"){
+        document.getElementById("div1").style.display = 'block';
+    }else{
+        document.getElementById("div1").style.display = 'none';
+    }
+}
+document.getElementById("dois").onclick = () => {
+    let displayAppears = document.getElementById("div2").style.display;
+    if(displayAppears === "none"){
+        document.getElementById("div2").style.display = 'block';
+    }else{
+        document.getElementById("div2").style.display = 'none';
+    }
+}
+document.getElementById("tres").onclick = () => {
+    let displayAppears = document.getElementById("div3").style.display;
+    if(displayAppears === "none"){
+        document.getElementById("div3").style.display = 'block';
+    }else{
+        document.getElementById("div3").style.display = 'none';
+    }
+}
+document.getElementById("quatro").onclick = () => {
+    let displayAppears = document.getElementById("div4").style.display;
+    if(displayAppears === "none"){
+        document.getElementById("div4").style.display = 'block';
+    }else{
+        document.getElementById("div4").style.display = 'none';
+    }
+}
